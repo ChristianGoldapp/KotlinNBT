ae8f3b9ee1aeecfaf423a5786e6c2ceee756ef93
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2021-07-06 17:00:32 +0200

    Add Maven publish to Gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.findKaptConfiguration
+
 plugins {
 	java
 	kotlin("jvm") version "1.5.10"
+	`maven-publish`
 }
 
 group = "io.github.mrpng"
@@ -18,4 +21,18 @@ dependencies {
 
 tasks.getByName<Test>("test") {
 	useJUnitPlatform()
+}
+
+publishing {
+
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			setUrl("https://maven.pkg.github.com/" + System.getenv("GITHUB_REPOSITORY"))
+			credentials {
+				username = System.getenv("GITHUB_ACTOR")
+				password = System.getenv("GITHUB_TOKEN")
+			}
+		}
+	}
 }
